94797700b7a1c885a9bf2ef0a19708e7a3af9eff
ethan <ethan@DESKTOP-9ITGI4S>
2022-11-13 00:30:42 -0500

    Added skeleton for tamagotchi.

--- tomotemp/src/application/Controllers/MainController.java
+++ tomotemp/src/application/Controllers/MainController.java
@@ -3,10 +3,14 @@ package application.Controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -20,13 +24,34 @@ import javafx.scene.layout.VBox;
 public class MainController implements Initializable{
 
 	@FXML
+    private Label currPointsValue;
+
+    @FXML
+    private Label healthValue;
+
+    @FXML
     private BorderPane homeTabPane;
+
+    @FXML
+    private StackPane imgStackPane;
+
+    @FXML
+    private Label levelValue;
 
     @FXML
     private AnchorPane mainAnchorPane;
 
     @FXML
     private Pane mainPane;
+
+    @FXML
+    private Label moodValue;
+
+    @FXML
+    private Label nameValue;
+
+    @FXML
+    private Label ovrPointsValue;
 
     @FXML
     private VBox tabsVBox;
@@ -56,18 +81,41 @@ public class MainController implements Initializable{
     private ImageView tomoStandingImg;
 
     @FXML
-    private StackPane viewStackPane;//Do not use
-    
+    private ListView<String> updatesList;
+
     @FXML
-    private StackPane imgStackPane;
+    private StackPane viewStackPane;
+    
+    static ObservableList<String> updateItems;
 
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
-    	tomoStandingEyesOpenImg.toFront();//use .toFront() to switch images/gifs
+    	tomoSleepingGIF.toFront();//use .toFront() to switch images/gifs
+    	updateItems =FXCollections.observableArrayList ("Program started.");
+    	updatesList.setItems(updateItems);
 	}
     
     public static void main(String[] args) {
     	//Do Stuff Here
     }
     
+    //Used to update list when outside function happens, for example: task added, task removed
+    public static void updateUpdatesList(String message) {
+    	updateItems.add(message);
+    }
+    
+    //Used to calculate points etc. when task is added
+    public static void taskAdded() {
+    	
+    }
+    
+    //Used to calculate points etc. when task is removed
+    public static void taskRemoved() {
+    	
+    }
+    
+    //Used to calculate points etc. when task is completed
+    public static void taskCompleted() {
+    	
+    }
 }
--- tomotemp/src/application/models/TasksModel.java
+++ tomotemp/src/application/models/TasksModel.java
@@ -2,12 +2,15 @@ package application.models;
 
 import java.util.List;
 
+import application.Controllers.MainController;
+
 public class TasksModel{
 	public static void addNewTaskToDB(List<String> currTask) {
 		String name = currTask.get(0);
 		String priority = currTask.get(1);
 		String date = currTask.get(2);
 		
+		MainController.updateUpdatesList("New task added.");
 		System.out.println("Task Added: " + currTask);
 	}
 	
@@ -16,6 +19,7 @@ public class TasksModel{
 		String priority = currTask.get(1);
 		String date = currTask.get(2);
 		
+		MainController.updateUpdatesList("Task removed.");
 		System.out.println("Task Deleted: " + currTask);
 	}
 }
